6bcaf56527788a3da68f77db1d808787387739a5
02camilasoares-hash <02camila.soares@gmail.com>
2026-03-19 23:03:30 -0300

    adicionando animações de entrada e saida

--- script.js
+++ script.js
@@ -3,11 +3,24 @@
 const latasMenor = document.querySelectorAll(".latas img:nth-child(2)");
 const slides = document.querySelectorAll(".slide");
 
+//contador: começa no 0. acr+1. 
+let contador = 0
+
 latasMenor.forEach(latinha=> {
     latinha.onclick = () => {
         const slideAtivo = document.querySelector(".slide.ativo");
         slideAtivo.classList.remove("ativo");
-        slides[1].classList.add("ativo");
+
+//condicionais -> if e else 
+
+    if(contador == 3) {
+        contador = 0;
+    } else {
+        contador = contador + 1;
+    }
+
+
+        slides[contador].classList.add("ativo");
     }
 })
 
